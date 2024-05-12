012c3bb87b1aba1c578312ddc87794996d1da908
taichi-hrgch <taichi1115.blue@gmail.com>
2024-05-12 19:05:50 +0900

    register画面へのリンク追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,11 +31,17 @@
                 <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
-
+            
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
+                </a>
+            @endif
+            
+            @if (Route::has('register'))
+                <a class="ml-4 underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('Register') }}
                 </a>
             @endif
 
